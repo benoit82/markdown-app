73771d45a1b16e0443dc21f29d5aa2f527a98cea
Benoit Dubus <benoit.dubus@abtasty.com>
2021-12-01 10:46:15 +0100

    login page init form update

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/Link'
 import Cours from '@models/Cours'
-import styles from '@styles/Home.module.css'
+import styles from '@styles/Login.module.css'
 import CustomHead from '@components/CustomHead'
 
 export default function Login() {
@@ -9,12 +10,30 @@ export default function Login() {
     const headProps = { pageTitle, metaName: 'LoginPage', metaContent: pageTitle };
 
   return (
-    <div className={styles.container}>
-    <CustomHead {...headProps} />
-      <main>
-        <h1>{pageTitle}</h1>
-        <p>notre application commence ici...☕️</p>
-      </main>
+    <div>
+        <CustomHead {...headProps} />
+        <main>
+            <h1>{pageTitle}</h1>
+            <form action="GET">
+                <div className="inputGroup">
+                    <label htmlFor="username">Utilisateur</label>
+                    <input type="text" name="username" id="username" />
+                </div>
+                <div className="inputGroup">
+                    <label htmlFor="password">mot de passe</label>
+                    <input type="password" name="password" id="password" />
+                </div>
+                <div className="inputGroup">
+                    <input type="submit" name="submitBtn" id="submitBtn" />
+                    <input type="button" name="resetBtn" id="resetBtn" value="Reset" />
+                </div>
+            </form>
+            <div className="helperGroup">
+                <Link href="/resetpassword">
+                    <a className={styles.lostPassword}>mot de passe perdu ?</a>
+                </Link>
+            </div>
+        </main>
     </div>
   )
 }
