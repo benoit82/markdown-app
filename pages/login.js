94ca18b0146262c377712c2e5565244d0a358f43
Benoit Dubus <benoit.dubus@abtasty.com>
2021-12-01 10:55:16 +0100

    update css

--- pages/login.js
+++ pages/login.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import Link from 'next/Link'
 import Cours from '@models/Cours'
 import styles from '@styles/Login.module.css'
+import stylesForm from '@styles/forms.module.css'
 import CustomHead from '@components/CustomHead'
 
 export default function Login() {
@@ -14,25 +15,25 @@ export default function Login() {
         <CustomHead {...headProps} />
         <main>
             <h1>{pageTitle}</h1>
-            <form action="GET">
-                <div className="inputGroup">
+            <form action="GET" className={stylesForm.form}>
+                <div className={stylesForm.inputGroup}>
                     <label htmlFor="username">Utilisateur</label>
                     <input type="text" name="username" id="username" />
                 </div>
-                <div className="inputGroup">
+                <div className={stylesForm.inputGroup}>
                     <label htmlFor="password">mot de passe</label>
                     <input type="password" name="password" id="password" />
                 </div>
-                <div className="inputGroup">
+                <div className={stylesForm.btnGroup}>
                     <input type="submit" name="submitBtn" id="submitBtn" />
                     <input type="button" name="resetBtn" id="resetBtn" value="Reset" />
                 </div>
+                <div className={stylesForm.helperGroup}>
+                    <Link href="/resetpassword">
+                        <a className={styles.lostPassword}>mot de passe perdu ?</a>
+                    </Link>
+                </div>
             </form>
-            <div className="helperGroup">
-                <Link href="/resetpassword">
-                    <a className={styles.lostPassword}>mot de passe perdu ?</a>
-                </Link>
-            </div>
         </main>
     </div>
   )
