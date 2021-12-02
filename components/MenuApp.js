d0dc6d53a5ae2706b623bcb58158d29ee821ac9c
Benoit Dubus <benoit.dubus@abtasty.com>
2021-12-02 09:55:53 +0100

    update menuapp (active link)

--- components/MenuApp.js
+++ components/MenuApp.js
@@ -1,6 +1,5 @@
 import Link from 'next/Link'
 import { useRouter } from 'next/router'
-import classNames from 'classnames'
 import styles from '@styles/MenuApp.module.css'
 
 const MenuApp = () => {
@@ -10,16 +9,16 @@ const MenuApp = () => {
         { title: 'Connexion', path: '/login' },
     ];
 
+    const {activeLink} = styles;
     return (
         <div className={styles.menuApp}>
             <ul className={styles.listGroup}>
                 {menu.map(({title, path}) => (
-                <li 
-                    key={`${title}${path}`}
-                    className={classNames({activeLink: router.pathname === path})}
-                >
+                <li key={`${title}${path}`}>
                         <Link href={path}>
-                            <a>{title}</a>
+                            <a className={`${ router.pathname === path ? 'activeLink' : '' }`}>
+                                {title}
+                            </a>
                         </Link>
                     </li>))}
             </ul>
