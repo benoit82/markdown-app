3b361db425e386a9c56bbc0e8833bb591f63ea65
Benoit Dubus <benoit.dubus@abtasty.com>
2021-12-03 10:34:12 +0100

    cours by id update

--- pages/cours/[id].js
+++ pages/cours/[id].js
@@ -3,38 +3,78 @@ import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 import styles from '@styles/cours/global.module.css'
 import PropTypes from 'prop-types'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
 
-const coursDisplay = ({mdContent, id}) => {
-    const pageTitle = `Cours - #${id}`;
+async function  getOneCours(coursId) {
+   const response =  await fetch('http://localhost:3000/api/server', {
+      method:'POST',
+      headers:{'content-type':'application/json'},
+      body:JSON.stringify({query:`{
+cours(coursId: "${coursId}") {
+    id
+    titre
+    contenuMd
+    resume
+    author {
+      id
+      name
+      email
+    }
+  }
+    }`})
+   })
+   const rsponseBody =  await response.json();
+   return rsponseBody.data.cours;
+}
+
+const coursDisplay = () => {
+    const pageTitle = `Cours -`;
     const headProps = { pageTitle, metaName: 'CoursPage', metaContent: pageTitle };
+    
+    const { query } = useRouter();
+
+    const [cours, setCours] = useState(null)
+
+    useEffect(() => {
+        const {id} = query;
+        if (id) getOneCours(id).then(setCours);
+    }, [query, setCours])
 
     return (
     <>
         <CustomHead {...headProps} />
         <main className={styles.mainFrame}>
+            {cours &&
+            <>
+            <div style={{padding: "2rem", backgroundColor: '#9F9F9F'}}>
+                <p>Cours id : {cours.id}</p>
+                <p>Titre : {cours.titre}</p>
+                <p>Resume : {cours.resume ? cours.resume : '<aucun>'}</p>
+                <p>Auteur : {cours.author.name} - {cours.author.email}</p>
+            </div>
             <ReactMarkdown remarkPlugins={[remarkGfm]}>
-                {mdContent}
+                {cours.contenuMd}
             </ReactMarkdown>
+            </>
+             }
         </main>
     </>
     )
 }
 
-export async function getServerSideProps({params}) {
-    // TODO API call here
-    const mdContent = `## Cours n°${params.id}`;
+// export async function getServerSideProps({params}) {
+//     const cours = await getOneCours(params.id);
 
-    return {
-        props: {
-            mdContent,
-            id: params.id,
-        }
-    }
-}
+//     return {
+//         props: {
+//             cours
+//         }
+//     }
+// }
 
 coursDisplay.propTypes = {
-    mdContent: PropTypes.string,
-    id: PropTypes.string,
+    cours: PropTypes.object,
 }
 
 export default coursDisplay;
