6f771b8fa7da50df98fff6e67176f2ad6761da51
Benoit Dubus <benoit.dubus@abtasty.com>
2021-12-03 09:52:40 +0100

    add homepage the cours list from db

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,37 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/Link'
 import styles from '@styles/Home.module.css'
 import CustomHead from '@components/CustomHead'
+import { gql, useQuery } from '@apollo/react-hooks';
+import { useEffect, useState } from 'react'
 
-export default function Home() {
+async function  getCours() {
+   const response =  await fetch('http://localhost:3000/api/server', {
+      method:'POST',
+      headers:{'content-type':'application/json'},
+      body:JSON.stringify({query:`{
+      drafts {
+        id
+        titre
+        contenuMd
+        resume
+      }
+    }`})
+   })
+   const rsponseBody =  await response.json();
+   return rsponseBody.data.drafts;
+}
 
+function Home() {
     const pageTitle = 'Accueil';
     const headProps = { pageTitle, metaName: 'HomePage', metaContent: pageTitle };
 
+    const [cours, setCours] = useState([]);
+
+    useEffect(() => {
+      getCours().then(setCours);
+    }, [setCours])
 
   return (
     <>
@@ -15,7 +39,18 @@ export default function Home() {
       <main>
         <h1>Markdown App</h1>
         <p>notre application commence ici...☕️</p>
+        <ul>
+          {cours.length > 0 && cours.map(({titre, id}) => 
+            (<li key={id}>
+              <Link href={`/cours/${id}`}>
+                <a>{titre}</a>
+              </Link>
+            </li>)
+          )}
+        </ul>
       </main>
     </>
   )
 }
+
+export default Home;
